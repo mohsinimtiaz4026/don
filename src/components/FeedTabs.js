d99116b05a8c5ccf0a815100f0abdd0259f5e71e
mohsinimtiaz4026 <businesstycon941@gmail.com>
2023-03-27 15:46:14 +0500

    makes changes

--- src/components/FeedTabs.js
+++ src/components/FeedTabs.js
@@ -13,10 +13,11 @@ import {
   IconButton,
 } from "@mui/material";
 import Iconify from "./Iconify";
-import Link from "next/link";
 import SvgIconStyle from "./SvgIconStyle";
 import useSettings from "../hooks/useSettings";
 import Typography from "@mui/material/Typography";
+import ReactPlayer from "react-player";
+import Image from "./Image";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -57,7 +58,7 @@ export default function BasicTabs() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  const {themeMode} = useSettings();
+  const { themeMode } = useSettings();
   return (
     <Box sx={{ width: "100%" }}>
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
@@ -78,29 +79,42 @@ export default function BasicTabs() {
               alignItems: "center",
               justifyContent: "space-between",
               px: 2,
-              py: 2
+              py: 2,
             }}
           >
             <Grid sx={{ display: "flex", alignItems: "center" }}>
               <Box>
                 <Avatar src="/logo/icandy-logo.png" />
               </Box>
-              <Box sx={{pl:1}}>
-                <Typography variant="h6" sx={{
-                  color: themeMode === "light" ? "#000" : "#fff"
-                }}>HottiGras</Typography>
-                <Typography sx={{
-                  color: "#92A1AF"
-                }}>@hottigras</Typography>
+              <Box sx={{ pl: 1 }}>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: themeMode === "light" ? "#000" : "#fff",
+                  }}
+                >
+                  HottiGras
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#92A1AF",
+                  }}
+                >
+                  @hottigras
+                </Typography>
               </Box>
-              <Box sx={{pl:1}}>
+              <Box sx={{ pl: 1 }}>
                 <SvgIconStyle src="/icons/ic_batch.svg" bgColor="#1DA1F2" />
               </Box>
             </Grid>
-            <Grid sx={{display:"flex",alignItems: "center"}}>
-              <Typography sx={{
-                color: "#92A1AF"
-              }}>5 min ago</Typography>
+            <Grid sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                sx={{
+                  color: "#92A1AF",
+                }}
+              >
+                5 min ago
+              </Typography>
               <IconButton aria-label="settings">
                 <Iconify icon="ic:outline-more-horiz" />
               </IconButton>
@@ -109,21 +123,28 @@ export default function BasicTabs() {
           <CardContent>
             <Typography variant="body2" color="text.secondary">
               Congras
-              <Box component="span" style={{ color: "#1DA1F2",textDecoration: "none",
-            padding: "0 4px",cursor: "pointer" }}>
+              <Box
+                component="span"
+                style={{
+                  color: "#1DA1F2",
+                  textDecoration: "none",
+                  padding: "0 4px",
+                  cursor: "pointer",
+                }}
+              >
                 @icandy
               </Box>
               for wining a Product Hunt T-shirt! DM us to claim your price.
             </Typography>
           </CardContent>
-          <CardMedia
-            component="video"
-            // height="220"
-            thumbnail="/images/image.png"
-            src="/images/_2.mp4"
-            alt="Video"
-            sx={{ p: 2 }}
-            controls
+          <CardMedia 
+          component="img"
+          height="250px"
+          src="/images/image.png"
+          style={{
+            padding: "0 12px",
+            borderRadius: "8px",
+          }}
           />
           <CardActions disableSpacing>
             <IconButton aria-label="add to favorites">
