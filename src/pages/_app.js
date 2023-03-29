fb56b4d30799abaa97c105976490bbeb668084f6
Syed <salizahid75@gmail.com>
2023-03-29 20:32:44 +0500

    Feat: Disable Zoom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -46,7 +46,11 @@ export default function MyApp(props) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        {/* <meta name="viewport" content="initial-scale=1, width=device-width" /> */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+        ></meta>
       </Head>
       <ReduxProvider store={store}>
         <PersistGate loading={null} persistor={persistor}>
