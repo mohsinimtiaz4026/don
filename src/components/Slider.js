3569be37e521458a3ebf7f417fe91acc324bdf41
Syed <salizahid75@gmail.com>
2023-04-05 00:25:01 +0500

    Fix: Scrollbar (Story & Body)

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,89 +1,89 @@
 // @components
-import Iconify from "./Iconify";
+import Iconify from './Iconify';
 // @mui
-import { Card, Avatar, IconButton, Typography, Grid } from "@mui/material";
+import {Card, Avatar, IconButton, Typography, Grid} from '@mui/material';
 // hooks
-import useSettings from "../hooks/useSettings";
-
+import useSettings from '../hooks/useSettings';
 
 const SliderContent = () => {
-  const { themeMode } = useSettings();
+  const {themeMode} = useSettings();
   const cardImages = [
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/two.jpeg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/two.jpeg',
+      profile: '/logo/profile.png',
     },
     {
-      imageUrl: "/images/one.jpg",
-      profile: "/logo/profile.png",
+      imageUrl: '/images/one.jpg',
+      profile: '/logo/profile.png',
     },
   ];
   return (
     <>
       <Grid
+        className="story-container"
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          overflowX: "auto",
-          width: {xs: "500px",md:"100%"}
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          overflowX: 'auto',
+          width: {xs: '500px', md: '100%'},
         }}
       >
         <div>
           <Card
             sx={{
-              width: { md: "140px", sm: "130px", xs: "120px" },
-              height: "160px",
-              position: "relative",
-              backgroundColor: themeMode === "light" ? "#fff" : "#1A0A23",
+              width: {md: '140px', sm: '130px', xs: '120px'},
+              height: '160px',
+              position: 'relative',
+              backgroundColor: themeMode === 'light' ? '#fff' : '#1A0A23',
               px: 2,
               pt: 2,
               pb: 4,
@@ -91,28 +91,29 @@ const SliderContent = () => {
           >
             <IconButton
               style={{
-                position: "absolute",
-                top: "15px",
-                left: "15px",
+                position: 'absolute',
+                top: '15px',
+                left: '15px',
                 backgroundColor:
-                  themeMode === "light"
-                    ? "#790DC2"
-                    : "rgba(255, 255, 255, 0.2)",
+                  themeMode === 'light'
+                    ? '#790DC2'
+                    : 'rgba(255, 255, 255, 0.2)',
               }}
             >
               <Iconify
                 icon="material-symbols:add"
                 sx={{
-                  color: "#fff",
+                  color: '#fff',
                 }}
               />
             </IconButton>
             <Typography
               style={{
-                position: "absolute",
-                bottom: "15px",
-                left: "15px",
-                color: themeMode === "light" ? "#000": "rgba(255, 255, 255, 0.35)",
+                position: 'absolute',
+                bottom: '15px',
+                left: '15px',
+                color:
+                  themeMode === 'light' ? '#000' : 'rgba(255, 255, 255, 0.35)',
               }}
             >
               Add a story
@@ -121,14 +122,14 @@ const SliderContent = () => {
         </div>
         {cardImages.map((item, index) => {
           return (
-            <div style={{ marginLeft: "30px" }} key={index}>
+            <div style={{marginLeft: '30px'}} key={index}>
               <Card
                 sx={{
-                  width: { md: "140px", sm: "130px", xs: "120px" },
-                  height: "160px",
-                  position: "relative",
+                  width: {md: '140px', sm: '130px', xs: '120px'},
+                  height: '160px',
+                  position: 'relative',
                   background: `linear-gradient(180deg, rgba(255, 80, 190, 0) 42.27%, #FF50BE 100%, #FF50BE 100%), url(${item.imageUrl})`,
-                  backgroundSize: "cover",
+                  backgroundSize: 'cover',
                   px: 2,
                   pt: 2,
                   pb: 4,
@@ -138,17 +139,17 @@ const SliderContent = () => {
                   src={item.profile}
                   alt="profile"
                   style={{
-                    position: "absolute",
-                    top: "15px",
-                    left: "15px",
+                    position: 'absolute',
+                    top: '15px',
+                    left: '15px',
                   }}
                 />
                 <Typography
                   style={{
-                    position: "absolute",
-                    bottom: "15px",
-                    left: "15px",
-                    color: "#fff",
+                    position: 'absolute',
+                    bottom: '15px',
+                    left: '15px',
+                    color: '#fff',
                   }}
                 >
                   Alogian Zoey
